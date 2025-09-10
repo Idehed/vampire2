0ebc051eae661d4049feffac1965459477a78779
Idehed <anton.idehed@telia.com>
2025-09-10 14:00:48 +0200

    movement

--- Assets/scrips/PlayerMovement.cs
+++ Assets/scrips/PlayerMovement.cs
@@ -5,15 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Animator animator;
     private Rigidbody2D _rb;
     private float _speed = 5f;
     public InputAction playerControls;
+    [SerializeField] private bool facingRight = true;
 
     Vector2 moveDirection = Vector2.zero;
 
     private void Start()
     {
        _rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void OnEnable()
@@ -30,11 +33,33 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         moveDirection = playerControls.ReadValue<Vector2>();
+        HandleAnimation();
+        HandleFlip();
     }
+
+    private void HandleAnimation()
+    {
+        bool isWalking = _rb.linearVelocity.x != 0 || _rb.linearVelocity.y != 0;
+        animator.SetBool("isWalking", isWalking);
+    }
+
     private void FixedUpdate()
     {
         _rb.linearVelocity = new Vector2(moveDirection.x * _speed, moveDirection.y * _speed);
     }
 
+    private void HandleFlip()
+    {
+        if (_rb.linearVelocity.x > 0 && facingRight == false)
+            Flip();
+        else if (_rb.linearVelocity.x < 0 && facingRight == true)
+            Flip();
+        
+    }
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
     
 }
